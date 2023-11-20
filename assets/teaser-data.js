99605c1282a1dab6e49618d18d0d7e9dfbf67502
diffusionAtlas <151505885+diffusionAtlas@users.noreply.github.com>
2023-11-20 15:49:01 +0800

    Add files via upload

---
+++ assets/teaser-data.js
@@ -0,0 +1,21 @@
+file_paths = {
+    "skiing-pink-sunset": "./assets/results/tuneavideo/man-skiing/pink-sunset.gif",
+    "skiing-wonder-woman": "./assets/results/tuneavideo/man-skiing/wonder-woman.gif",
+    "skiing-spiderman-beach": "./assets/results/tuneavideo/man-skiing/spiderman-beach.gif",
+
+    "car-moving-snow": "./assets/results/tuneavideo/car-turn/car-snow.gif",
+    "car-moving-cartoon": "./assets/results/tuneavideo/car-turn/car-cartoon.gif",
+    "car-moving-porsche": "./assets/results/tuneavideo/car-turn/porsche-beach.gif",
+
+    "basketball-bond": "./assets/results/tuneavideo/man-basketball/bond.gif",
+    "basketball-lego": "./assets/results/tuneavideo/man-basketball/lego.gif",
+    "basketball-astronaut": "./assets/results/tuneavideo/man-basketball/astronaut.gif",
+
+    "lion-vangogh": "./assets/results/tuneavideo/lion-roaring/lion-vangogh.gif",
+    "lion-tiger": "./assets/results/tuneavideo/lion-roaring/tiger-roar.gif",
+    "lion-wolf": "./assets/results/tuneavideo/lion-roaring/wolf-nyc.gif",
+
+    "rabbit-alone": "./assets/results/tuneavideo/rabbit-watermelon/rabbit.gif",
+    "rabbit-puppy": "./assets/results/tuneavideo/rabbit-watermelon/puppy.gif",
+    "rabbit-cat": "./assets/results/tuneavideo/rabbit-watermelon/cat.gif",
+}
