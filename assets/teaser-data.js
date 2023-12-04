1ce95b8ef5f7ea166d1b7f31e7387b040be05017
diffusionAtlas <151505885+diffusionAtlas@users.noreply.github.com>
2023-12-04 15:56:23 +0800

    Update teaser-data.js

--- assets/teaser-data.js
+++ assets/teaser-data.js
@@ -2,4 +2,7 @@ file_paths = {
     "boat-yacht": "./assets/results/boat/yacht.gif",
     "boat-yellowYacht": "./assets/results/boat/yellowYacht.gif",
     "boat-warshipCartoon": "./assets/results/boat/warshipCartoon.gif",
+
+    "kite-surf-monkey": "./assets/results/kite-surf/hot_spring_monkey.gif",
+    "kite-surf-bikini": "./assets/results/kite-surf/pool_bikini.gif",
 }
