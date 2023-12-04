21678eeadcceae41ed03fad388280bd9fdad57c0
diffusionAtlas <151505885+diffusionAtlas@users.noreply.github.com>
2023-12-04 15:32:01 +0800

    Create teaser-data.js

---
+++ assets/teaser-data.js
@@ -0,0 +1,5 @@
+file_paths = {
+    "boat-yacht": "./assets/results/boat/yacht.gif",
+    "boat-yellowYacht": "./assets/results/boat/yellowYacht.gif",
+    "boat-warshipCartoon": "./assets/results/boat/warshipCartoon.gif",
+}
